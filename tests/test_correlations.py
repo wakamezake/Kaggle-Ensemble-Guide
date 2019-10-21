56595468e5cc329d25d979ebac3b5a4ae26d2e0a
wakamezake <hotwater1367@gmail.com>
2019-10-21 13:48:59 +0900

    toxic_corr

--- tests/test_correlations.py
+++ tests/test_correlations.py
@@ -3,13 +3,19 @@ from pathlib import Path
 
 import pytest
 
-from src.correlations import corr
+from src.correlations import corr, corr_toxic
 
 root_path = Path("../samples")
+toxic_path = root_path / "ToxicCommentClassificationChallenge"
 
 method_1_path = root_path / "method1.csv"
 method_2_path = root_path / "method2.csv"
 method_3_path = root_path / "method3.csv"
+
+model_1_path = toxic_path / "Logistic_regression_with_words_and" \
+                            "_char_n-grams.csv"
+model_2_path = toxic_path / "NB_SVM_strong_linear_baseline.csv"
+model_3_path = toxic_path / "pooled-gru-fasttext.csv"
 
 
 @pytest.mark.parametrize('first_path, second_path',
@@ -18,3 +24,11 @@ method_3_path = root_path / "method3.csv"
                                        method_3_path], 2))
 def test_corr(first_path, second_path):
     corr(first_path, second_path)
+
+
+@pytest.mark.parametrize('first_path, second_path',
+                         combinations([model_1_path,
+                                       model_2_path,
+                                       model_3_path], 2))
+def test_corr_toxic(first_path, second_path):
+    corr_toxic(first_path, second_path)
